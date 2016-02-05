15882caded8ff5e6344d570a4232f7d8981f451b
David R. Smith <drsmith8@wisc.edu>
2016-02-05 13:03:44 -0500

    deleted obsolete fdfsignal.py

--- fdp/__init__.py
+++ fdp/__init__.py
@@ -8,4 +8,8 @@ Created on Wed Nov 25 19:34:56 2015
 import sys
 from classes import fdp
 
+# Change fdp module mapping from fdp.__init__.py module to fdp.Fdp() object
+# This change enables:
+# >>> import fdp
+# >>> nstx = fdp.nstx
 sys.modules[__name__] = fdp.Fdp()
