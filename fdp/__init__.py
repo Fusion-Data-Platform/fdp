4102f41c79ed547ad4ff83cf03c6bb22fbaac75c
David R. Smith <drsmith8@wisc.edu>
2016-02-05 23:28:24 -0500

    added libs/MDSplus...egg and appended to sys.path in fdp/__init__.py

--- fdp/__init__.py
+++ fdp/__init__.py
@@ -6,13 +6,14 @@ Created on Wed Nov 25 19:34:56 2015
 """
 
 import sys
+import os
 
 #from . import classes
 from .classes.fdp import Fdp
 from . import methods
 
-# make subpackages importable
-#__all__ = ['classes', 'methods']
+MDSplusEgg = os.path.join(os.path.dirname(os.path.abspath(__name__)), 'libs', 'MDSplus-7.0.62-py2.7.egg')
+sys.path.append(MDSplusEgg)
 
 # The 'fdp' package is remapping from fdp.__init__.py to classes.fdp.Fdp() object.
 # The remapping provides this functionality:
