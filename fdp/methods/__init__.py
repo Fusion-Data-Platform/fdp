3cbd9d78856bcf4c3182ca81d7427810d2735f59
David R. Smith <drsmith8@wisc.edu>
2016-06-22 11:39:39 -0400

    added methods/utilities.py

--- fdp/methods/__init__.py
+++ fdp/methods/__init__.py
@@ -8,6 +8,6 @@ Created on Thu Oct 29 10:20:43 2015
 
 @author: ktritz
 """
-from plot import plot
+from .plot import plot
 
 __all__ = ['plot']
---
+++ fdp/methods/utilities.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 22 11:18:01 2016
+
+@author: drsmith
+"""
+
+def listSignals(obj):
+    pass
+
+def listAttributes(obj):
+    pass
+
+def listContainers(obj):
+    pass
+
+def listMethods(obj):
+    pass
