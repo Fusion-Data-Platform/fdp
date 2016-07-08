a948891c0167036887b5cb9c47666a443134cdfa
David R. Smith <drsmith8@wisc.edu>
2016-07-08 16:01:31 -0400

    depricated powerspectrum() in methods/nstxu/bes/fft.py.  Instead use Fft.binavg_psd and Fft.binavg_logpsd.

--- fdp/methods/nstxu/bes/fft.py
+++ fdp/methods/nstxu/bes/fft.py
@@ -21,7 +21,6 @@ def fft(obj, *args, **kwargs):
         offsetmin = kwargs.pop('offsetminimum')
     else:
         offsetmin = True
-    
     if isSignal(obj):
         return Fft(obj, offsetminimum=offsetmin, *args, **kwargs)
     elif isContainer(obj):
@@ -44,9 +43,9 @@ def plotfft(signal, fmax=None, *args, **kwargs):
     ax = fig.add_subplot(1,1,1)
     pcm = ax.pcolormesh(sigfft.time, 
                         sigfft.freq, 
-                        sigfft.psd.transpose(), 
+                        sigfft.logpsd.transpose(), 
                         cmap=plt.cm.YlGnBu)
-    pcm.set_clim([sigfft.psd.max()-100, sigfft.psd.max()-20])
+    pcm.set_clim([sigfft.logpsd.max()-100, sigfft.logpsd.max()-20])
     #ax.set_ylim([0,200])
     cb = plt.colorbar(pcm, ax=ax)
     cb.set_label(r'$10\,\log_{10}(|FFT|^2)$ $(V^2/Hz)$')
@@ -63,30 +62,32 @@ def powerspectrum(signal, fmax=None, *args, **kwargs):
     """
     Calcualte bin-averaged power spectrum
     """
-    if not isSignal(signal):
-        warn("Method valid only at signal-level", FdpWarning)
-        return
-    if 'tmin' not in kwargs:
-        kwargs['tmin'] = 0.25
-    if 'tmax' not in kwargs:
-        kwargs['tmax'] = 0.26
-    if not fmax:
-        fmax = 250
-    sigfft = fft(signal, *args, **kwargs)
-    psd = np.square(np.absolute(sigfft.fft))
-    # bin-averaged PSD, in dB
-    sigfft.bapsd = 10*np.log10(np.mean(psd, axis=0))
-    fig=plt.figure()
-    ax=fig.add_subplot(1,1,1)
-    ax.plot(sigfft.freq, sigfft.bapsd)
-    ax.set_ylabel(r'$10\,\log_{10}(|FFT|^2)$ $(V^2/Hz)$')
-    ax.set_xlim([0,fmax])
-    ax.set_xlabel('Frequency (kHz)')
-    ax.set_title('{} | {} | {} | {}-{} s'.format(
-                 sigfft.shot, 
-                 sigfft.parentname.upper(), 
-                 sigfft.signalname.upper(),
-                 kwargs['tmin'],
-                 kwargs['tmax']))
-    return sigfft
+    msg = "Depricated.  Use Fft.binavg_psd and Fft.binavg_logpsd."
+    warn(msg, FdpWarning)
+#    if not isSignal(signal):
+#        warn("Method valid only at signal-level", FdpWarning)
+#        return
+#    if 'tmin' not in kwargs:
+#        kwargs['tmin'] = 0.25
+#    if 'tmax' not in kwargs:
+#        kwargs['tmax'] = 0.26
+#    if not fmax:
+#        fmax = 250
+#    sigfft = fft(signal, *args, **kwargs)
+#    psd = np.square(np.absolute(sigfft.fft))
+#    # bin-averaged PSD, in dB
+#    sigfft.bapsd = 10*np.log10(np.mean(psd, axis=0))
+#    fig=plt.figure()
+#    ax=fig.add_subplot(1,1,1)
+#    ax.plot(sigfft.freq, sigfft.bapsd)
+#    ax.set_ylabel(r'$10\,\log_{10}(|FFT|^2)$ $(V^2/Hz)$')
+#    ax.set_xlim([0,fmax])
+#    ax.set_xlabel('Frequency (kHz)')
+#    ax.set_title('{} | {} | {} | {}-{} s'.format(
+#                 sigfft.shot, 
+#                 sigfft.parentname.upper(), 
+#                 sigfft.signalname.upper(),
+#                 kwargs['tmin'],
+#                 kwargs['tmax']))
+#    return sigfft
     
