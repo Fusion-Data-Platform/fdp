b0e8c40f31c2e98f1373064a59ff39363fc193a7
David R. Smith <drsmith8@wisc.edu>
2016-07-16 11:19:41 -0400

    starting GUI development with classes/gui.py, examples/gui_example.py, and methods/nstxu/overviewgui.py

---
+++ examples/gui_example.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 16 11:10:19 2016
+
+@author: drsmith
+"""
+
+import fdp
+
---
+++ fdp/classes/gui.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 16 11:10:48 2016
+
+@author: drsmith
+"""
+
