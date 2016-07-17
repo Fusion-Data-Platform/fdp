604f7c8bd67d5cb7ef89fbd167ecdf93ad75aa70
David R. Smith <drsmith8@wisc.edu>
2016-07-17 00:16:44 -0400

    successfully embedded Matplotlib figure in TKinter frame

--- fdp/classes/gui.py
+++ fdp/classes/gui.py
@@ -6,17 +6,15 @@ Created on Sat Jul 16 11:10:48 2016
 """
 
 import sys
-import ttk
 if sys.version_info[0] < 3:
     import Tkinter as tk
 else:
     import tkinter as tk
+import ttk
 
-import matplotlib
-matplotlib.use('TkAgg')
+import matplotlib as mpl
+mpl.use('TkAgg')
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-import matplotlib.pyplot as plt
-plt.ioff()
 
 
 class BaseGui(object):
@@ -37,17 +35,19 @@ class BaseGui(object):
         self.tkroot = tk.Tk()
         self.tkroot.title(title)
         
-        self.mainframe = ttk.Frame(master=self.parent)
-        
-        self.figure = plt.figure()
+        self.figure = mpl.figure.Figure()
         self.axes = self.figure.add_subplot(1, 1, 1)
         
-        self.canvas = FigureCanvasTkAgg(self.figure, master=self.tkroot)
-        self.canvas.show()
-        self.canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+        self.controlframe = ttk.Frame(master=self.tkroot, borderwidth=5, 
+                                     width=100, height=100, relief='ridge')
+        self.controlframe.pack(side='left')
+        self.figureframe = ttk.Frame(master=self.tkroot, borderwidth=5, 
+                                     width=100, height=100, relief='ridge')
+        self.figureframe.pack(side='right')
         
-        self.controlframe = None
-        self.figureframe = None
+        self.canvas = FigureCanvasTkAgg(self.figure, master=self.figureframe)
+        self.canvas.show()
+        self.canvas.get_tk_widget().pack(side=tk.RIGHT, fill=tk.BOTH, expand=1)
         
         self.tkroot.mainloop()
         
