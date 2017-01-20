d1719ef66d5e3646792e9ae51488b8edb4e2bf64
David R. Smith <drsmith8@wisc.edu>
2017-01-20 10:49:02 -0500

    added conda and pip convenience scripts for environment management

---
+++ scripts/conda-generate-package-list.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# generate 'conda-package-list.txt' file
+conda list --export > conda-package-list.txt
+
---
+++ scripts/pip-generate-requirements.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# generate 'requirements.txt' file
+pip freeze --all > requirements.txt
+
